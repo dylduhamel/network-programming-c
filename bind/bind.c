099613ec82b1564fc9fdf51922298b8e9ec22547
dylduhamel <duhadm19@alumni.wfu.edu>
2024-02-27 14:26:07 -0500

    Added bind syscall

---
+++ bind/bind.c
@@ -0,0 +1,37 @@
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+
+int main(int argc, char *argv[])
+{
+  struct addrinfo hints, *res;
+  int sockfd;
+  int status_getaddrinfo, status_bind;
+
+  memset(&hints, 0, sizeof(hints));
+  hints.ai_family = AF_UNSPEC; /* Use IPv4 or IPv6 */
+  hints.ai_socktype = SOCK_STREAM;
+  hints.ai_flags = AI_PASSIVE; /* Fill in IP of local machine for me */
+
+  if ((status_getaddrinfo = getaddrinfo(NULL, "3490", &hints, &res)) != 0)
+  {
+    fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(status_getaddrinfo));
+    return 1;
+  }
+
+  /* Make a socket */
+  sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+
+  /* Bind socket to the port on host machine */
+  if ((status_bind = bind(sockfd, res->ai_addr, res->ai_addrlen)) != 0)
+  {
+    fprintf(stderr, "bind: %s\n", gai_strerror(status_bind));
+    return 2;
+  }
+
+  return 0;
+}
